0ea4a388921af57d8512362f97420f6ecf08cb3f
Gravestar <olegator2016@mail.ru>
2020-03-02 09:34:51 +0300

    Fixed canvas

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 "use strict";
-let canvas = document.getElementById("gameCanvas");
-let ctx = getContext("2d");
+let canvas = document.getElementById('gameCanvas');
+let ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
